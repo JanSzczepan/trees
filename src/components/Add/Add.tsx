5c1d9290bf134cde6bc8bc5da185ad5c2da865de
Jan Szczepan <janszczepan62@gmail.com>
2023-02-14 22:25:23 +0100

    Add tree button - redirect user if not logged in

--- src/components/Add/Add.tsx
+++ src/components/Add/Add.tsx
@@ -1,11 +1,14 @@
 import { Button } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import { useUserContext } from '../../contexts/userContext'
 
 function Add() {
+   const { user } = useUserContext()
+
    return (
       <section>
          <h3 className='mb-3'>Be the hero, add the tree...</h3>
-         <Link to='add-tree'>
+         <Link to={user.email ? 'add-tree' : 'login'}>
             <Button
                variant='primary'
                size='lg'
